7f5320165547c5cca2f332c47f35bdf0df37bc1b
Chris Milliano <MillianoC@gmail.com>
2017-10-31 00:02:44 -0500

    CHORE: Turn off forceSSL to try getting heroku working

--- server.js
+++ server.js
@@ -21,24 +21,24 @@ if(process.env.NODE_ENV != "production") {
 
 /******************** FORCE SSL ********************/
 
-if(process.env.NODE_ENV == "production") {
-
-	// If an incoming request uses a protocol other than HTTPS,
-	// redirect that request to the same url but with HTTPS
-	const forceSSL = function() {
-		return function (req, res, next) {
-			if (req.headers['x-forwarded-proto'] !== 'https') {
-				return res.redirect(['https://', req.get('Host'), req.url].join(''));
-			}
-			next();
-		}
-	}
-
-
-	// Instruct the app to use the forceSSL middleware
-	app.use(forceSSL());
-	api.use(forceSSL());
-}
+// if(process.env.NODE_ENV == "production") {
+//
+// 	// If an incoming request uses a protocol other than HTTPS,
+// 	// redirect that request to the same url but with HTTPS
+// 	const forceSSL = function() {
+// 		return function (req, res, next) {
+// 			if (req.headers['x-forwarded-proto'] !== 'https') {
+// 				return res.redirect(['https://', req.get('Host'), req.url].join(''));
+// 			}
+// 			next();
+// 		}
+// 	}
+//
+//
+// 	// Instruct the app to use the forceSSL middleware
+// 	app.use(forceSSL());
+// 	api.use(forceSSL());
+// }
 
 
 /******************** SETUP API ROUTES ********************/
